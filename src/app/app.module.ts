7eb70b0c5c36a11a986db8941f91a2511d5e036d
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-18 10:42:14 +0300

    Run style:fix and lint:fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core'
-import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppMaterialModule } from './app-material.module'
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component';
-import { ManagerModule } from './manager/manager.module';
-import { InventoryModule } from './inventory/inventory.module';
-import { PosModule } from './pos/pos.module';
+import { AppComponent } from './app.component'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
+import { PosModule } from './pos/pos.module'
 import { UserModule } from './user/user.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule, ManagerModule, InventoryModule, PosModule, UserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
