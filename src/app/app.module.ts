9998f04b09142521f9326c1fba6ae408846928cb
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-18 10:19:54 +0300

    Generate inventory module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppMaterialModule } from './app-material.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
-import { ManagerModule } from './manager/manager.module'
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule, ManagerModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule, ManagerModule, InventoryModule],
   providers: [],
   bootstrap: [AppComponent],
 })
