26376b68828c599cb798fe378993011b42c68a0a
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-18 10:21:26 +0300

    Generate user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
 import { ManagerModule } from './manager/manager.module';
 import { InventoryModule } from './inventory/inventory.module';
-import { PosModule } from './pos/pos.module'
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule, ManagerModule, InventoryModule, PosModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule, ManagerModule, InventoryModule, PosModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
