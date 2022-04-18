22fdbe55637063ba5a5a9b532b19a6ddc0c70205
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-18 11:08:17 +0300

    Configured a wildcard route for PageNotFoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
   // pathMatch ensures that the this very instance of home
   // route will be rendered as the landing experience.
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
 ]
 
 @NgModule({
