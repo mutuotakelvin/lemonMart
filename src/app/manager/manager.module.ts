1c488a6cc667a912a208472aa05d01af0d07d05f
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-19 14:30:08 +0300

    Added userManagement page

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -5,10 +5,11 @@ import { Routes } from '@angular/router'
 import { AppMaterialModule } from './../app-material.module'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { ManagerRoutingModule } from './manager-routing.module'
-import { ManagerComponent } from './manager.component'
+import { ManagerComponent } from './manager.component';
+import { UserManagementComponent } from './user-management/user-management.component'
 
 @NgModule({
-  declarations: [ManagerHomeComponent, ManagerComponent],
+  declarations: [ManagerHomeComponent, ManagerComponent, UserManagementComponent],
   imports: [CommonModule, ManagerRoutingModule, AppMaterialModule],
 })
 export class ManagerModule {}
