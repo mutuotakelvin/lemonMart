78b32dc06d821417baa63055af7c12ed09a25c60
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-19 13:44:38 +0300

    Updated manager path with loadChildren

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
-import { managerModuleRoutes } from './manager/manager.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
@@ -10,7 +9,10 @@ const routes: Routes = [
   // route will be rendered as the landing experience.
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'manager', children: managerModuleRoutes },
+  {
+    path: 'manager',
+    loadChildren: () => import('./manager/manager.module').then((m) => m.ManagerModule),
+  },
   { path: '**', component: PageNotFoundComponent },
 ]
 
