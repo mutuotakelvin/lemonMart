528f951b7804dd42c95f19b84e8ee73cf3a5c183
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-19 13:12:47 +0300

    Add managerModuleRoutes as children routes to manager path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
+import { managerModuleRoutes } from './manager/manager.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   // route will be rendered as the landing experience.
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'manager', children: managerModuleRoutes },
   { path: '**', component: PageNotFoundComponent },
 ]
 
