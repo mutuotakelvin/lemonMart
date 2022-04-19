47db6a042674b4ac1630442f3ab69c48448bc54b
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-19 14:51:35 +0300

    create user profile component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
-import { UserRoutingModule } from './user-routing.module'
+import { UserRoutingModule } from './user-routing.module';
+import { ProfileComponent } from './profile/profile.component'
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [CommonModule, UserRoutingModule],
 })
 export class UserModule {}
