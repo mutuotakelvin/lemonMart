3ec34fe0c3bca5baa74fc42989cf21b2dd2b61f9
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-19 13:37:16 +0300

    Remove manager module from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
 import { InventoryModule } from './inventory/inventory.module'
-import { ManagerModule } from './manager/manager.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 import { PosModule } from './pos/pos.module'
 import { UserModule } from './user/user.module'
@@ -21,7 +20,6 @@ import { UserModule } from './user/user.module'
     AppRoutingModule,
     BrowserAnimationsModule,
     AppMaterialModule,
-    ManagerModule,
     InventoryModule,
     PosModule,
     UserModule,
