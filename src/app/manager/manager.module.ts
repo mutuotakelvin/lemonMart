9a54c102b8a23c63509fd146ef1589897a7f7011
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-19 13:07:49 +0300

    Remove ManagerRoutingModule

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,13 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
-import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
 
 @NgModule({
-  declarations: [
-    ManagerHomeComponent
-  ],
-  imports: [CommonModule, ManagerRoutingModule],
+  declarations: [ManagerHomeComponent],
+  imports: [CommonModule],
 })
 export class ManagerModule {}
