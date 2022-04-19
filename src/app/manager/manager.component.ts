17a27b72a9606054f1cadc172cbe1db0da439506
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-19 14:25:18 +0300

    Add manager page and import AppMaterialModule to manager module

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-manager',
   template: `
-    <p>
-      manager works!
-    </p>
+    <mat-toolbar color="accent">
+      <a mat-button routerLink="/manager/home" routerLinkActive="active-link">
+        Manger's Dashboard
+      </a>
+      <a mat-button routerLink="/manager/users" routerLinkActive="active-link">
+        User Management
+      </a>
+      <a mat-button routerLink="/manager/receipts" routerLinkActive="active-link">
+        Receipt Lookup
+      </a>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
   `,
   styles: [
-  ]
+    `
+      div[fxLayout] {
+        margin-top: 32px;
+      }
+    `,
+    `
+      .active-link {
+        font-weight: bold;
+        border-bottom: 2px solid #005005;
+      }
+    `,
+  ],
 })
 export class ManagerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 
+import { AppMaterialModule } from './../app-material.module'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
-import { ManagerRoutingModule } from './manager-routing.module';
+import { ManagerRoutingModule } from './manager-routing.module'
 import { ManagerComponent } from './manager.component'
 
 @NgModule({
   declarations: [ManagerHomeComponent, ManagerComponent],
-  imports: [CommonModule, ManagerRoutingModule],
+  imports: [CommonModule, ManagerRoutingModule, AppMaterialModule],
 })
 export class ManagerModule {}
