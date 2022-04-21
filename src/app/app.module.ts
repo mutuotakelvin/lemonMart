3042e23f1a15cb20a31ef229b31ae7278d9de78c
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-21 09:22:54 +0300

    Update with newly added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,7 @@ import { AppMaterialModule } from './app-material.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
-import { InventoryModule } from './inventory/inventory.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
-import { PosModule } from './pos/pos.module'
-import { UserModule } from './user/user.module'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
@@ -20,9 +17,6 @@ import { UserModule } from './user/user.module'
     AppRoutingModule,
     BrowserAnimationsModule,
     AppMaterialModule,
-    InventoryModule,
-    PosModule,
-    UserModule,
     HttpClientModule,
     FlexLayoutModule,
   ],
