30a1ccddc1c831385b6c30fce5d90db8f119c2fa
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-24 18:28:25 +0300

    Extend the abstract AuthService

--- src/app/auth/inmemory.service.ts
+++ src/app/auth/inmemory.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core'
 
+import { AuthService } from './auth.service'
+
 @Injectable()
-export class InMemoryAuthService {
-  constructor() {}
+export class InMemoryAuthService extends AuthService {
+  constructor() {
+    super()
+    console.warn(
+      "You're using the InMemoryAuthService. Do not use this service in production."
+    )
+  }
 }
