fafb3f6cfa1dfae85b7210e2622d37cd943494df
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-24 18:43:52 +0300

    Refactor

--- src/app/auth/inmemory.service.ts
+++ src/app/auth/inmemory.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core'
+import { sign } from 'fake-jwt-sign'
+import { Observable, of, throwError } from 'rxjs'
 
-import { AuthService } from './auth.service'
+import { User } from '../user/user/user'
+import { Role } from './auth.enum'
+import { AuthService, IAuthStatus, IServerAuthResponse } from './auth.service'
 
 @Injectable()
 export class InMemoryAuthService extends AuthService {
+  protected authProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse> {
+    email = email.toLowerCase()
+    if (!email.endsWith('@test.com')) {
+      return throwError('Failed to login! Email needs to end with @test.com.')
+    }
+    const authStatus = {
+      isAuthenticated: true,
+      userId: this.defaultUser._id,
+      userRole: email.includes('cashier')
+        ? Role.Cashier
+        : email.includes('clerk')
+        ? Role.Clerk
+        : email.includes('manager')
+        ? Role.Manager
+        : Role.None,
+    } as IAuthStatus
+    this.defaultUser.role = authStatus.userRole
+    const authResponse = {
+      accessToken: sign(authStatus, 'secret', {
+        expiresIn: '1h',
+        algorithm: 'none',
+      }),
+    } as IServerAuthResponse
+    return of(authResponse)
+  }
+  protected transformJwtToken(token: IAuthStatus): IAuthStatus {
+    return token
+  }
+  protected getCurrentUser(): Observable<User> {
+    return of(this.defaultUser)
+  }
   constructor() {
     super()
     console.warn(
