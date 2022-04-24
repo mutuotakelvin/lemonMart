fdd76c280c188e78b6a1637d0820b0c0aa5fe813
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-24 12:57:38 +0300

    Define abstract methods

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -29,6 +29,13 @@ export interface IAuthService {
 }
 @Injectable()
 export abstract class AuthService implements IAuthService {
+  protected abstract authProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse>
+  protected abstract transformJwtToken(token: unknown): IAuthStatus
+  protected abstract getCurrentUser(): Observable<User>
+
   constructor() {}
   readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus)
   readonly currentUser$ = new BehaviorSubject<IUser>(new User())
