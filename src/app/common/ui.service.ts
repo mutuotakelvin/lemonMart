336f1f18420e3a6c372545987fde572b3b6be672
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-25 16:45:41 +0300

    Implemented the showDialog()

--- src/app/common/ui.service.ts
+++ src/app/common/ui.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core'
-import { MatDialog } from '@angular/material/dialog'
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog'
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar'
+import { Observable } from 'rxjs'
+
+import { SimpleDialogComponent } from './simple-dialog.component'
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +12,21 @@ export class UiService {
   constructor(private snackBar: MatSnackBar, private dialog: MatDialog) {}
   showToast(message: string, action = 'close', config?: MatSnackBarConfig) {
     this.snackBar.open(message, action, config || { duration: 700 })
+  }
+  showDialog(
+    title: string,
+    content: string,
+    okText: 'ok',
+    cancelText?: string,
+    customerConfig?: MatDialogConfig
+  ): Observable<boolean> {
+    const dialogRef = this.dialog.open(
+      SimpleDialogComponent,
+      customerConfig || {
+        width: '300px',
+        data: { title, content, okText, cancelText },
+      }
+    )
+    return dialogRef.afterClosed()
   }
 }
