18683f01c06c8ddfc35ca41ca76f0c9a044c507f
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-25 14:25:05 +0300

    Refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
-import { LoginComponent } from './login.component'
+import { LoginComponent } from './login/login.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
